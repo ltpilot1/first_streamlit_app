241daf878bcc6473c238ff627ded37ddfc525957
ltpilot1 <124319658+ltpilot1@users.noreply.github.com>
2023-02-07 14:15:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.text("\U0001F600")
-streamlit.title('	:banana: :strawberry: Build Your Own Fruit Smoothie :kiwi: :grapes: ')
+streamlit.title(':banana::strawberry:Build Your Own Fruit Smoothie:kiwifruit::grapes:')
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
