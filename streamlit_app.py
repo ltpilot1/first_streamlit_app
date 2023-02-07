d5a67e4154ba3824ba591e004e4ec60b801d2514
ltpilot1 <124319658+ltpilot1@users.noreply.github.com>
2023-02-07 14:50:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')  #this puts the name of the fru
 # Let's put a pick list here so they can pick the fruit they want to include 
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)) this is the whole list with no default values
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries']) this creates 2 default values of fruits
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
