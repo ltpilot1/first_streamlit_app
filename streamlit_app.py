ae548142ba02a70cf6ced7147da27bd28124c7a3
ltpilot1 <124319658+ltpilot1@users.noreply.github.com>
2023-02-07 14:19:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit
 
 streamlit.title("My Mom's New Healthy Diner")
 
-streamlit.header(':bowl_with_spoon: Breakfast Favorites')
-streamlit.text(':green_salad: Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.header('Breakfast Favorites')
+streamlit.text(':bowl_with_spoon: Omega 3 & Blueberry Oatmeal')
+streamlit.text(':green_salad: Kale, Spinach & Rocket Smoothie')
 streamlit.text(':egg: Hard-Boiled Free-Range Egg')
 streamlit.text(':avacado: :toast: Avacado Toast')
 
