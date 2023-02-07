ea4150fa396a33d99cf5bc418a7172f9d1967765
ltpilot1 <124319658+ltpilot1@users.noreply.github.com>
2023-02-07 14:32:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,11 @@ streamlit.header(':banana::mango: Build Your Own Fruit Smoothie :kiwifruit::grap
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')  #this puts the name of the fruit as opposed to the number of the row corresponding to the fruit in the picker list
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)) --this puts the number of the row corresponding to the fruit
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.fruit))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
