0ff2f380628c17a2df91fb12cd5daf7456324453
ltpilot1 <124319658+ltpilot1@users.noreply.github.com>
2023-02-20 11:28:54 -0500

    Update streamlit_app.py
    changed my_data_row to my_data_rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,6 +61,6 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchone()   # changing this to my_data_row returns just one row
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)  # changing this to my_data_row returns just one row
