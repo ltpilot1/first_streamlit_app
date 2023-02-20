ac3ad2fe914c5ab271259be724f804ed7ae323cd
ltpilot1 <124319658+ltpilot1@users.noreply.github.com>
2023-02-20 16:10:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,3 +64,7 @@ my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_rows = my_cur.fetchall()   # changed the variable from my_data_row and the function my_cur.fetchone() which returns just one row to fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)  # updated the variable from my_data_row
+
+# Allow the end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to add', fruit_add)
+streamlit.write('Thanks for adding', fruit_add)
