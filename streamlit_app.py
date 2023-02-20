6bfe2d76805f8d88080a51a7f6022567aed45f96
ltpilot1 <124319658+ltpilot1@users.noreply.github.com>
2023-02-20 11:13:24 -0500

    Update streamlit_app.py
    Added the code to query the trial account metadata after adding the secret configuration.

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,3 +48,11 @@ streamlit.dataframe(fruityvice_normalized)
 # The requirements.txt file you just added to your project tells Streamlit what libraries you plan to use in your project so it can add them in advance.
 # The line shown below will tell this py file to use the library you added to the project. 
 import snowflake.connector 
+
+# Let's query our trial account metadata
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
