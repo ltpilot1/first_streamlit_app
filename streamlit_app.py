15ab0c917d16531ed09107038e00f596d10ffcc2
ltpilot1 <124319658+ltpilot1@users.noreply.github.com>
2023-02-20 11:00:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,6 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-#The requirements.txt file you just added to your project tells Streamlit what libraries you plan to use in your project so it can add them in advance.
+# The requirements.txt file you just added to your project tells Streamlit what libraries you plan to use in your project so it can add them in advance.
 # The line shown below will tell this py file to use the library you added to the project. 
 import snowflake.connector 
